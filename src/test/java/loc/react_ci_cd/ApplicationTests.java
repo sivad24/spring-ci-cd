45207adc918548403d6caad4b27629ff49338b8d
Siva Dhanush <dhanush@Sivas-MacBook-Air.local>
2026-03-25 00:13:58 +0530

    reactive spring setup
    reactive setup

---
+++ src/test/java/loc/react_ci_cd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package loc.react_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
